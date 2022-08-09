ba41b47455bd49ee73c16b6c1fab99db47e435af
lllypik <blesphoton@gmail.com>
2022-08-09 17:46:46 +0300

    Элементы управления реализованы командами

--- TextEDTR/MainWindow.xaml.cs
+++ TextEDTR/MainWindow.xaml.cs
@@ -26,7 +26,8 @@ namespace TextEDTR
         {
             InitializeComponent();
         }
-        //Обратботка типа шрифта
+      
+        //Обратботка типа шрифта 
         private void fontList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (textBox != null)
@@ -35,6 +36,7 @@ namespace TextEDTR
                 textBox.FontFamily = new FontFamily(fontName);
             }
         }
+       
         //Обработка размера шрифта
         private void fontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -44,37 +46,42 @@ namespace TextEDTR
                 textBox.FontSize = fontSize;
             }
         }
+
         //Обработка толщины шрифта
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void FontBoldExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             if (textBox.FontWeight == FontWeights.Normal)
                 textBox.FontWeight = FontWeights.Bold;
             else textBox.FontWeight = FontWeights.Normal;
         }
+
         //Обработка наклона шрифта
-        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        private void FontItalicExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             if (textBox.FontStyle == FontStyles.Normal)
                 textBox.FontStyle = FontStyles.Italic;
             else textBox.FontStyle = FontStyles.Normal;
         }
+       
         //Обработка подчеркивания шрифта
-        private void ToggleButton_Click_1(object sender, RoutedEventArgs e)
+        private void FontUnderlineExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             if (textBox.TextDecorations.Count == 0)
                 textBox.TextDecorations.Add(TextDecorations.Underline);
             else textBox.TextDecorations.Remove(TextDecorations.Underline[0]);
         }
+        
         //Обработка цвета шрифта (черный)
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void FontColorBlackExecuted(object sender, RoutedEventArgs e)
         {
             if (textBox != null)
             {
                 textBox.Foreground = Brushes.Black;
             }
         }
+        
         //Обработка цвета шрифта (красный)
-        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        private void FontColorRedExecuted (object sender, RoutedEventArgs e)
         {
             if (textBox != null)
             {
@@ -82,24 +89,27 @@ namespace TextEDTR
             }
         }
 
-        //
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        // Открытие файла 
+        private void OpenExecuted(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
             if (openFileDialog.ShowDialog()==true)     textBox.Text = File.ReadAllText(openFileDialog.FileName);
         }
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        //Сохранение файла
+        private void SaveExecuted(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
             if (saveFileDialog.ShowDialog() == true) File.WriteAllText(saveFileDialog.FileName, textBox.Text);
         }
 
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        //Закрытие программы 
+        private void CloseExecuted (object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
+
     }
 }
---
+++ TextEDTR/MyCommands.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TextEDTR
+{
+    class MyCommands
+    {
+
+        public static RoutedCommand FontBold { get; set; }
+
+        public static RoutedCommand FontItalic { get; set; }
+
+        public static RoutedCommand FontUnderline { get; set; }
+
+        public static RoutedCommand FontSize { get; set; }
+
+        public static RoutedCommand FontType { get; set; }
+
+        //public static RoutedCommand FontColorBlack { get; set; }
+        public static RoutedUICommand FontColorBlack { get; set; }
+
+        public static RoutedCommand FontColorRed { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputBold = new InputGestureCollection();
+            inputBold.Add(new KeyGesture(Key.B, ModifierKeys.Control, "CTRL+B"));
+            FontBold = new RoutedCommand("FontBold", typeof(MyCommands), inputBold);
+
+            InputGestureCollection inputItalic = new InputGestureCollection();
+            inputItalic.Add(new KeyGesture(Key.I, ModifierKeys.Control, "CTRL+I"));
+            FontItalic = new RoutedCommand("FontItalic", typeof(MyCommands), inputItalic);
+
+            InputGestureCollection inputUnderline = new InputGestureCollection();
+            inputUnderline.Add(new KeyGesture(Key.U, ModifierKeys.Control, "CTRL+U"));
+            FontUnderline = new RoutedCommand("FontItalic", typeof(MyCommands), inputUnderline);
+
+            //FontColorBlack = new RoutedCommand("FontColorBlack", typeof(MyCommands));
+            FontColorBlack = new RoutedUICommand("Black","FontColorBlack", typeof(MyCommands));
+
+            FontColorRed = new RoutedCommand("FontColorRed", typeof(MyCommands));
+
+        }
+    }
+}
